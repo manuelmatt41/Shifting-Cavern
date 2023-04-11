6ab55f77c504cb386bc69bc58f365a5f8ed713e6
Manuel Marín <manuelmat41@gmail.com>
2023-04-12 01:02:17 +0200

    feat: Añadido AnimationTree y AnimationPlayer para el manejo de las animaciones dentro del juego

--- Entities/Player/Player.cs
+++ Entities/Player/Player.cs
@@ -1,8 +1,20 @@
 using Godot;
-using System;
 
 public partial class Player : CharacterBody2D
 {
+    [Export]
+    public float MoveSpeed { get; set; } = 100f;
+
+    [Export]
+    public Vector2 StartPosition { get; set; } = new Vector2(0, 1);
+    private AnimationTree animationTree;
+
+    public override void _Ready()
+    {
+        this.animationTree = this.GetNode<AnimationTree>("AnimationTree");
+        animationTree.Set("parameters/Idle/blend_position", StartPosition);
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         var inputDirection = new Vector2(
@@ -10,6 +22,17 @@ public partial class Player : CharacterBody2D
             Input.GetActionStrength("Down") - Input.GetActionStrength("Up")
         );
 
-        GD.Print(inputDirection);
+        UpdateAnimationParameters(inputDirection);
+
+        this.Velocity = inputDirection * MoveSpeed;
+        this.MoveAndSlide();
+    }
+
+    public void UpdateAnimationParameters(Vector2 moveInto)
+    {
+        if (moveInto == Vector2.Zero) return;
+
+        animationTree.Set("parameters/Idle/blend_position", moveInto);
+        animationTree.Set("parameters/Walk/blend_position", moveInto);
     }
 }
