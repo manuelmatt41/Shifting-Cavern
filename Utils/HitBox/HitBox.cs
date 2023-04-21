151b94ef8a857159c730d7df81114f0636c8434a
Manuel Marin <mmarin@novossistemas.com>
2023-04-21 10:12:38 +0200

    feat: añadido documentacion de la clase HitBox

--- Utils/HitBox/HitBox.cs
+++ Utils/HitBox/HitBox.cs
@@ -1,27 +1,43 @@
 using Godot;
-using System;
 
 public partial class HitBox : Area2D
 {
+    /// <summary>
+    /// Daño que va realizar el area
+    /// </summary>
     [Export]
     public double Damage { get; set; } = 1;
 
+    /// <summary>
+    /// Forma de la colision que va actuar con el resto de areas
+    /// </summary>
     private CollisionShape2D collisionShape;
 
+    /// <summary>
+    /// <c>Timer</c> que comprueba el tiempo que se va a desactivar las colisiones
+    /// </summary>
     private Timer disableTimer;
-
+    /// <summary>
+    /// Funcion integrada de Godot que se ejecuta al crear el nodo en la escena, se usa para iniciar las variables de nodos subyacentes de <c>HurtBox</c>
+    /// </summary>
     public override void _Ready()
     {
         this.collisionShape = this.GetNode<CollisionShape2D>("CollisionShape2D");
         this.disableTimer = this.GetNode<Timer>("DisableTimer");
     }
 
+    /// <summary>
+    /// Desactiva las colisiones y activa <c>disableTimer</c>
+    /// </summary>
     public void TempDisable()
     {
         this.collisionShape.SetDeferred("disable", true);
         this.disableTimer.Start();
     }
 
+    /// <summary>
+    /// Evento que se ejecuta al terminar <c>disableTimer</c> para activar las colisiones
+    /// </summary>
     private void OnHitBoxTimerTimeOut()
     {
         this.collisionShape.SetDeferred("disable", false);
