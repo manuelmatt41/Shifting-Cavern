36abe8533ed6fc51a8e8fb7dc9c19a2866bae4f6
Manuel Marin <mmarin@novossistemas.com>
2023-05-05 14:57:36 +0200

    fix: retoques a Sign

--- InteractiveObjects/Sign/Sign.cs
+++ InteractiveObjects/Sign/Sign.cs
@@ -3,5 +3,25 @@ using System;
 
 public partial class Sign : RigidBody2D
 {
+    public BalloonTextControl BalloonTextControl { get; private set; }
 
+    public override void _Ready()
+    {
+        this.BalloonTextControl = this.GetNode<BalloonTextControl>(nameof(this.BalloonTextControl));
+    }
+    private void OnArea2DBodyEntered(Node2D body)
+    {
+        if (body.IsInGroup("Player"))
+        {
+            this.BalloonTextControl.Start();
+        }
+    }
+
+    private void OnArea2DBodyExited(Node2D body)
+    {
+        if (body.IsInGroup("Player"))
+        {
+            this.BalloonTextControl.Reset();
+        }
+    }
 }
