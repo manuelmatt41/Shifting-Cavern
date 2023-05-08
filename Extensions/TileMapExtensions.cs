a16d3cc6f65e79ff55b6d00684fea8bb2269fa25
Manuel Marin <mmarin@novossistemas.com>
2023-05-08 16:29:25 +0200

    feat: clase para extensiones para TileMap

---
+++ Extensions/TileMapExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Godot;
+
+public static class TileMapExtensions
+{
+    public static void StartMapComponents(this TileMap tileMap)
+    {
+        foreach (var child in tileMap.GetChildren())
+        {
+            if (child is EnemySpawner enemySpawner)
+            {
+                enemySpawner.Start();
+            }
+        }
+    }
+
+    public static void StopMapComponents(this TileMap tileMap)
+    {
+        foreach (var child in tileMap.GetChildren())
+        {
+            if (child is EnemySpawner enemySpawner)
+            {
+                enemySpawner.Stop();
+            }
+        }
+    }
+}
